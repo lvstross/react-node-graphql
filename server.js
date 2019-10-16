54f82dcf39d50812d435991fe3c6173b3deca9d0
Levi Gonzales <lvstross@gmail.com>
2019-10-15 21:09:59 -0600

    Started react graphql node app.

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+require('dotenv').config({ path: 'variables.env' });
+const Recipe = require('./models/Recipe');
+const User = require('./models/User');
+
+// Bring in GraphQL - Express Middleware
+const { graphiqlExpress, graphqlExpress } = require('apollo-server-express');
+const { makeExecutableSchema } = require('graphql-tools');
+
+const { typeDefs } = require('./schema');
+const { resolvers } = require('./resolvers');
+
+// Create Schema
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+
+// Connects to database
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log('DB connected'))
+  .catch(e => console.error(e));
+
+// Initializes application
+const app = express();
+
+app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }))
+
+
+// Connect schemas with GraphQL
+app.use(
+  '/graphql',
+  bodyParser.json(),
+  graphqlExpress({
+    schema,
+    context: {
+      Recipe,
+      User,
+    }
+  }),
+);
+const PORT = process.env.PORT || 4444;
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
