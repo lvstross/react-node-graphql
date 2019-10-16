79928ad4c76869cd3095ef75937b5b89b552e278
Levi Gonzales <lvstross@gmail.com>
2019-10-16 16:55:00 -0600

    Added react frontend for the client. Started out first query from the client to test.

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+
 require('dotenv').config({ path: 'variables.env' });
 const Recipe = require('./models/Recipe');
 const User = require('./models/User');
@@ -23,6 +25,13 @@ mongoose
 
 // Initializes application
 const app = express();
+
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 
 app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }))
 
